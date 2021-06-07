fac4ab8074595eddf6bcf9bb6bfe79755d53bbb1
dylan green <dylgrn.github.io>
2021-06-06 21:06:14 -0500

    delete button funcitoning

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 const public = path.join(__dirname, '/public');
 
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +24,26 @@ app.post('/api/notes', (req, res) => {
     let nId = (sNote.length).toString();
     nNote.id = nId;
     sNote.push(nNote)
+    fs.writeFileSync("./db/db.json", JSON.stringify(sNote));
+    res.json(sNote);
+})
+
+app.post("/api/notes/:id", (req, res)=> {
+    let sNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(sNote[Number(req.params.id)]);
+})
+
+app.delete("/api/notes/:id", (req, res)=> {
+    let sNote = JSON.parse(fs.readFileSync("./db/db.json"));
+    let thisId = req.params.id;
+    let nId = 0;
+    sNote = sNote.filter(thisNote => {
+        return thisNote.id != thisId;
+    })
+    for (thisNote of sNote) {
+        thisNote.id = nId.toString();
+        nId++;
+    }
     fs.writeFileSync("./db/db.json", JSON.stringify(sNote));
     res.json(sNote);
 })
